e6e141f4ce7d652d75203dba6d89fdf99c92aa7a
余凡 <albert@cform.io>
2016-02-21 14:58:40 +0800

    🐋  添加模拟的藉由路由触发的 Google Analytics Service 机制
    Signed-off-by: 余凡 <albert@cform.io>

---
+++ imports/services/google-analytics.js
@@ -0,0 +1,5 @@
+export default googleAnalytics = (context) => {
+  console.info(`We're pretending to send information to Google Analytics...`);
+  console.log(context.path);
+  console.info(`...that's awesome, right?`);
+};
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,8 @@ import StaticLayout from '../imports/layouts/StaticLayout.jsx';
 import PostList from '../imports/containers/PostList.js';
 import PostShow from '../imports/containers/PostShow.js';
 
+import googleAnalytics from '../imports/services/google-analytics.js';
+
 FlowRouter.route('/', {
   // name is optional, but it could be handy if you want to refer to this route
   // programatically, e.g. `FlowRouter.go('home')`
@@ -27,3 +29,5 @@ FlowRouter.route('/post/:_id', {
     });
   },
 });
+
+FlowRouter.triggers.enter([googleAnalytics]);
