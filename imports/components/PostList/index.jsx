88ade75263d9b21e57e153ca169d1cbdf8ab40da
余凡 <albert@cform.io>
2016-02-26 11:12:03 +0800

    🔅  添加 JSDoc 文档解析服务
    Signed-off-by: 余凡 <albert@cform.io>

--- imports/components/PostList/index.jsx
+++ imports/components/PostList/index.jsx
@@ -1,35 +1,47 @@
 import React, {Component} from 'react';
 // import {FlowRouter} from 'meteor/kadira:flow-router-ssr';
-import {FormattedNumber, FormattedPlural} from 'react-intl';
 export * from './PostListLoading.jsx';
 
+/**
+ * PostList 组件用于渲染 Posts 列表
+ */
 export default class PostList extends Component {
+  /**
+   * 组件构造器
+   * @param {object} params - `this.props`
+   */
   constructor() {
     super();
-    this.state = {
-      unreadCount: 1,
-    };
   }
 
+  /**
+   * @private
+   */
   render() {
-    const posts = this.props.posts.map(({_id, title}) => (
-      <li key={_id}>
-        <a href={FlowRouter.path('post', {_id})}>{title}</a>
-      </li>
-    ));
-
-    const {unreadCount} = this.state;
-
     return (
       <section>
         <header>
           <h2>Post List</h2>
-          <FormattedNumber value={unreadCount} /> {' '}
-          <FormattedPlural value={unreadCount} one="message" other="messages" />
         </header>
 
-        <ul>{posts}</ul>
+        <ul>{this.props.posts.map(this.renderPostEntry)}</ul>
       </section>
     );
+  }
+
+  /**
+   * 渲染单独的 *Post Entry*，用于抽象列表渲染逻辑
+   *
+   * ```
+   * <ul>{this.props.posts.map(this.renderPostEntry)}</ul>
+   * ```
+   *
+   * @param {object} post - `post` 对象。解构使用 `_id` 和 `title` 两个 fields.
+   * @return {PostEntry} 一个 `PostEntry` 组件
+   */
+  renderPostEntry({_id, title}) {
+    return <li key={_id}>
+      <a href={FlowRouter.path('post', {_id})}>{title}</a>
+    </li>;
   }
 }
